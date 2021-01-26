c61013ebde7f21ddd09e02a46fc71ef9cc85e1e7
guoliang <guoliang@baiing.cn>
2021-01-26 23:12:43 +0800

    second commit

---
+++ WXNSDK5/WXNSDK.swift
@@ -0,0 +1,14 @@
+//
+//  WXNSDK.swift
+//  WXNSDK5
+//
+//  Created by Bright on 2021/1/26.
+//
+
+import Foundation
+
+public class Running {
+    public class func start() {
+        print("GOGOGOG")
+    }
+}
---
+++ WXNSDK5/WXNSDK5.h
@@ -0,0 +1,18 @@
+//
+//  WXNSDK5.h
+//  WXNSDK5
+//
+//  Created by Bright on 2021/1/26.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WXNSDK5.
+FOUNDATION_EXPORT double WXNSDK5VersionNumber;
+
+//! Project version string for WXNSDK5.
+FOUNDATION_EXPORT const unsigned char WXNSDK5VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WXNSDK5/PublicHeader.h>
+
+
